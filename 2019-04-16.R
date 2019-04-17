4b088c8232841a981166c17ea6d0a46c630eb95d
yurisa2 <yuri@sa2.com.br>
2019-04-17 15:44:12 -0300

    Com HW entregue

--- 2019-04-16.R
+++ 2019-04-16.R
@@ -1,11 +1,12 @@
 library(imager)
-
+library(magick)
+setwd("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi")
 A <- load.image("img/nico.jpg")
 PBA <- grayscale(A)
 par(mfrow=c(2,2))
 plot(PBA)
 hist(PBA)
-PBAb <- threshold(PBA,0.58)
+PBAb <- threshold(PBA,(150/255))
 plot(PBAb)
 
 
@@ -27,24 +28,45 @@ plot(C_Original)
 
 # Convolucao Folha
 folha <- load.image("img/folha2.jpg")
-folha <- grayscale(folha)
+folha <-grayscale(folha)
 kernel <- as.cimg(matrix(c(-1,-1,-1,-1,8,-1,-1,-1,-1),nrow=3,ncol=3))
-convolutada <- convolve(folha,kernel)
-plot(convolutada)
+conv_folha <- convolve(folha,kernel)
+conv_folha[,,,1] <- ifelse(conv_folha[,,,1] < 0, conv_folha*0, conv_folha*1)
+conv_folha[,,,1] <- ifelse(conv_folha[,,,1] > 1, 1, conv_folha*1)
+plot(conv_folha, main = "Convolução Folha")
 
 # Convolucao Nico
 nico <- load.image("img/nico.jpg")
 nico <- grayscale(nico)
-nico <- threshold(nico,0.58)
+nico_t <- threshold(nico,(150/255))
 kernel <- as.cimg(matrix(c(-1,-1,-1,-1,8,-1,-1,-1,-1),nrow=3,ncol=3))
-convolutada <- convolve(nico,kernel)
-plot(convolutada)
+nico_mask <- convolve(nico,kernel)
+nico_mask[,,,1] <- ifelse(nico_mask[,,,1] < 0, nico_mask*0, nico_mask*1)
+nico_mask[,,,1] <- ifelse(nico_mask[,,,1] > 1, 1, nico_mask*1)
+
+nico_t_mask <- convolve(nico_t,kernel)
+nico_t_mask[,,,1] <- ifelse(nico_t_mask[,,,1] < 0, nico_t_mask*0, nico_t_mask*1)
+nico_t_mask[,,,1] <- ifelse(nico_t_mask[,,,1] > 1, 1, nico_t_mask*1)
+
+par(mfrow=c(2,2))
+plot(nico, main = "Img Original")
+plot(nico_mask, main = "Mascara Img Original")
+plot(nico_t, main = "Detecção de limiar (>150)")
+plot(nico_t_mask, main = "Mascara c/ Det. Limiar")
 
 # Convolucao Canal R Laranjas
 laranjas <- load.image("img/laranjas.jpg")
-laranjas_R <- channels(laranjas,1)
+laranjas_R <- as.matrix(channels(laranjas,1)[[1]])
 kernel <- as.cimg(matrix(c(-1,-1,-1,-1,8,-1,-1,-1,-1),nrow=3,ncol=3))
-laranjas_R <- convolve(laranjas_R[[1]],kernel)
-plot(laranjas_R)
-laranjas[,,,1] <- laranjas_R
-plot(laranjas)
+laranjas_R_conv_result <- laranjas
+laranjas_R <- as.cimg(laranjas_R)
+laranjas_R_conv <- convolve(laranjas_R,kernel)
+laranjas_R_conv[,,,1] <- ifelse(laranjas_R_conv[,,,1] < 0, laranjas_R_conv*0, laranjas_R_conv*1)
+laranjas_R_conv[,,,1] <- ifelse(laranjas_R_conv[,,,1] > 1, 1, laranjas_R_conv*1)
+laranjas_R_conv_result[,,,1] <- laranjas_R_conv
+
+par(mfrow=c(2,2))
+plot(laranjas, main = "Imagem Original")
+plot(laranjas_R, main = "Canal Vermelho (R)")
+plot(laranjas_R_conv, main = "Convolução do canal Vermelho")
+plot(laranjas_R_conv_result, main = "Imagem reconstituida")
