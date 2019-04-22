a90d7de0502dee64aa19aaef568da7a10f778dfc
yurisa2 <yuri@sa2.com.br>
2019-04-22 17:01:14 -0300

    Manipulando a regressão

--- 2019-04-16q2.R
+++ 2019-04-16q2.R
@@ -4,7 +4,7 @@ setwd("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi")
 im <- load.image("img/placa.jpg")
 im <- grayscale(im)
 
-k <- 2
+# k <- 2
 # Convolucao Placa
 # kernel <- as.cimg(matrix(c(-k/8,-k/8,-k/8,-k/8,k+1,-k/8,-k/8,-k/8,-k/8),nrow=3,ncol=3))
 # kernel <- as.cimg(matrix(c(-k/8,-k/8,-k/8,-k/8,k+1,-k/8,-k/8,-k/8,1),nrow=3,ncol=3))
@@ -12,14 +12,41 @@ k <- 2
 
 
 df <- as.data.frame(im)
+# head(df)
+
+line_mean <- aggregate(df$value,by=list(x=df$x), FUN=mean)
+colnames(line_mean)[2] <- "media"
+
+# plot(line_mean)
+# head(line_mean)
 
 
-m <- lm(value ~ x + y,data=df) #linear trend
-# summary(m)
 
+line_mean_lm <- lm(df$value ~ df$x)
 
-im <- im-fitted(m)
-hist(renorm(im))
+# head(df)
+
+df2 <- df
+
+df2$value <- df$value + (df$value * (-1 * line_mean_lm$coefficients[2] * df$x *  3))
+
+# head(df2)
+# hist(im)
+# hist(as.cimg(df2))
+
+line_mean_lm2 <- lm(df2$value ~ df2$x)
+
+plot(im)
+plot(as.cimg(df2))
+
+thres2 <- renorm(as.cimg(df2))
+thres2 <- as.cimg(thres2)
+thres2 <- renorm(thres2)
+
+hist(thres2)
+thres3 <- threshold(thres2, thr = 120)
+
+plot(thres3)
 
 w_size <- 6
 
