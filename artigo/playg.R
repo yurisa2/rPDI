b989708ad214bb8c0913d631e8af5685ee59250d
yurisa2-i3wm <yuri@sa2.com.br>
2019-05-06 08:11:35 -0300

    transf

--- artigo/playg.R
+++ artigo/playg.R
@@ -1,37 +1,53 @@
 library(imager)
-setwd("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/")
+setwd("/home/yurisa2/rPDI/artigo")
 
 source(file="afis_fuzzysis/include/functions.R")
+# 
+# doente <- load.image("test/Parasitized/C39P4thinF_original_IMG_20150622_105335_cell_9.png")
+# img_df <- as.data.frame(doente)
+# 
+# img_df <- img_df[img_df$value!=0,] #MELHORAR AS FUNCOES DE RECORTE
+# 
+# doente_f <- convert_image(doente)
 
-# doente <- load.image("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/test/Parasitized/C39P4thinF_original_IMG_20150622_105335_cell_9.png")
-# doente_df <- as.data.frame(doente)
-# doente_r <- subset(doente_df$value,doente_df$cc==1)
-# doente_r <- doente_r[doente_r!=0]
-# doente_g <- subset(doente_df$value,doente_df$cc==2)
-# doente_g <- doente_g[doente_g!=0]
-# doente_b <- subset(doente_df$value,doente_df$cc==3)
-# doente_b <- doente_b[doente_b!=0]
+convert_image <- function(img,dataset_type_bin = 0) {
+  img_df <- as.data.frame(img)
+
+  img_r <- subset(img_df$value,img_df$cc==1)
+  img_r <- img_r[img_r!=0] #MELHORAR AS FUNCOES DE RECORTE
+  img_g <- subset(img_df$value,img_df$cc==2)
+  img_g <- img_g[img_g!=0]
+  img_b <- subset(img_df$value,img_df$cc==3)
+  img_b <- img_b[img_b!=0]
+  feature_img <- dataset_type_bin
+
+  feature_img <- c(feature_img,mean(img_r))
+  feature_img <- c(feature_img,mean(img_g))
+  feature_img <- c(feature_img,mean(img_b))
+
+  feature_img <- c(feature_img,min(img_r))
+  feature_img <- c(feature_img,min(img_g))
+  feature_img <- c(feature_img,min(img_b))
+  # 
+  # feature_img <- c(feature_img,(max(img_r) - min(img_r)))
+  # feature_img <- c(feature_img,(max(img_g) - min(img_g)))
+  # feature_img <- c(feature_img,(max(img_b) - min(img_b)))
+
+  feature_img <- c(feature_img,length(img_df$value[img_df$value < quantile(img_df$value)[2]]))
 
 
-
-
-# sadia <- load.image("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/test/Uninfected/C2NThinF_IMG_20150604_114631_cell_187.png")
-# sadia_df <- as.data.frame(sadia)
-# sadia_r <- subset(sadia_df$value,sadia_df$cc==1)
-# sadia_r <- sadia_r[sadia_r!=0]
-# sadia_g <- subset(sadia_df$value,sadia_df$cc==2)
-# sadia_g <- sadia_g[sadia_g!=0]
-# sadia_b <- subset(sadia_df$value,sadia_df$cc==3)
-# sadia_b <- sadia_b[sadia_b!=0]
-# par(mfrow=c(3,2))
-# hist(sadia_r, xlim=c(0,1))
-# hist(doente_r, xlim=c(0,1))
-# hist(sadia_g, xlim=c(0,1))
-# hist(doente_g, xlim=c(0,1))
-# hist(sadia_b, xlim=c(0,1))
-# hist(doente_b, xlim=c(0,1))
-
-
+  # FAZER AQUI UMA SERIE COM CALCULOS NORMALIZADOS
+  # FAZER AQUI UMA SERIE COM CALCULOS GRAYSCALE
+  
+  # img_g_df <- as.data.frame(grayscale(img))
+  # img_g_df <- img_g_df[img_g_df!=0]
+  # 
+  # feature_img <- c(feature_img,mean(img_g_df))
+  # feature_img <- c(feature_img,min(img_g_df))
+  
+  
+  return(feature_img)
+}
 
 
 process_images <- function(path_set, dataset_type_bin, limit = 20, start = 0) {
@@ -43,30 +59,8 @@ process_images <- function(path_set, dataset_type_bin, limit = 20, start = 0) {
   for (i in img_list) {
     if(counter > start) {
     img <- load.image(paste0(path_set,i))
-    img_df <- as.data.frame(img)
-    # REMOVER o RECORTE AQUI, POSSIVELMENTE FAZER AS CONTAS PARA SABER
-    # SE A INFECCAO TEM ALguM TOM DIFERENTE DE 0
-    img_r <- subset(img_df$value,img_df$cc==1)
-    img_r <- img_r[img_r!=0] #MELHORAR AS FUNCOES DE RECORTE
-    img_g <- subset(img_df$value,img_df$cc==2)
-    img_g <- img_g[img_g!=0]
-    img_b <- subset(img_df$value,img_df$cc==3)
-    img_b <- img_b[img_b!=0]
-    feature_img <- dataset_type_bin
 
-    feature_img <- c(feature_img,as.array(mean(img_r)))
-    feature_img <- c(feature_img,as.array(mean(img_g)))
-    feature_img <- c(feature_img,as.array(mean(img_b)))
-    feature_img <- c(feature_img,as.array(min(img_r)))
-    feature_img <- c(feature_img,as.array(min(img_g)))
-    feature_img <- c(feature_img,as.array(min(img_b)))
-    feature_img <- c(feature_img,as.array((max(img_r) - min(img_r))))
-    feature_img <- c(feature_img,as.array((max(img_g) - min(img_g))))
-    feature_img <- c(feature_img,as.array((max(img_b) - min(img_b))))
-
-    # FAZER AQUI UMA SERIE COM CALCULOS NORMALIZADOS
-    # FAZER AQUI UMA SERIE COM CALCULOS GRAYSCALE
-    # Transformar os dados de imagens em funcao.
+    feature_img <- convert_image(img,dataset_type_bin)
 
     table_img <- rbind(table_img,feature_img)
 
@@ -81,24 +75,24 @@ process_images <- function(path_set, dataset_type_bin, limit = 20, start = 0) {
   return(as.data.frame(table_img))
 }
 
-training_1 <- process_images("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/cell_images/Parasitized/",1, limit = 100)
-training_0 <- process_images("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/cell_images/Uninfected/",0, limit = 100)
+training_1 <- process_images("cell_images/Parasitized/",1, limit = 100)
+training_0 <- process_images("cell_images/Uninfected/",0, limit = 100)
 
-total_training <- rbind(as.matrix(training_0),as.matrix(training_1))
+total_training <- rbind(as.matrix(training_1),as.matrix(training_0))
 total_training <- as.data.frame(total_training)
 
 
-actual_1 <- process_images("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/cell_images/Parasitized/",1, limit = 500, start = 101)
-actual_0 <- process_images("/home/yurisa2/lampstack-5.6.22-0/apache2/htdocs/rpdi/artigo/cell_images/Uninfected/",0, limit = 500, start = 101)
-total_actual <- rbind(as.matrix(actual_0),as.matrix(actual_1))
+actual_1 <- process_images("cell_images/Parasitized/",1, limit = 500, start = 101)
+actual_0 <- process_images("cell_images/Uninfected/",0, limit = 500, start = 101)
+total_actual <- rbind(as.matrix(actual_1),as.matrix(actual_0))
 total_actual <- as.data.frame(total_actual)
 
 possb_feat <- 2:ncol(total_actual)
 nbin <- 1
 
-# weights_type <- "dynamic"
 
-results <- result_matrix(total_training,total_actual,possb_feat,nbin)
-# str(results)
+results <- result_matrix(total_training,total_actual,possb_feat,nbin,weights = "dynamic", method = "only_1")
+summary(results)
 
-confusionMatrix(factor(results$Eval1),factor(results$Benchmark),positive = "1")
+confusionMatrix(factor(results$Eval1),factor(results$Benchmark))
+
