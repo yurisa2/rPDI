036975e7e4cc8a19af3997c0e46355220eafb953
yurisa2 <yuri@sa2.com.br>
2019-05-20 11:05:22 -0300

    Cortadas

--- artigo/sat/playg.R
+++ artigo/sat/playg.R
@@ -1,4 +1,6 @@
-PATH <- "/home/yurisa2/Downloads/itupararanga"
+PATH <- "C:/Bitnami/wampstack-7.0.16-1/apache2/htdocs/rPDI/artigo/sat"
+PATH_IMG <- "C:/Users/Administrator/Documents/landsat"
+
 setwd(PATH)
 
 
@@ -9,21 +11,13 @@ substrRight <- function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
 }
 
-
-
-file_list <- list.files(PATH, recursive = TRUE)
-dir_list <- list.dirs(PATH)
-
-for(dir in dir_list) {
-  dir.create(paste0("represa/",dir))
-}
-
+file_list <- list.files(PATH_IMG, recursive = TRUE, full.names = TRUE)
 for(i in file_list){
   if(substrRight(i, 3) == "tif" || substrRight(i, 3) == "TIF") {
     rasto <- raster(i)
     represa <- load.image(i)
     represa <- extent(252000,275000,-2621000,-2608000)
     rasto_represa <- crop(rasto,represa)
-    writeRaster(rasto_represa,paste0(i,"_represa.tif"),options=c('TFW=YES'))
+    writeRaster(rasto_represa,paste0("represa/",basename(i)),options=c('TFW=NO'))
   }
 }
