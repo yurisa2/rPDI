272ccb0da55d9d46bb47faebde46877b657fd324
yurisa2-i3wm <yuri@sa2.com.br>
2019-05-20 23:11:09 -0300

    Calculating

--- artigo/sat/functions.R
+++ artigo/sat/functions.R
@@ -1,6 +1,8 @@
 
 name_parser <- function(name) {
-  # substr(x, first, last)
+  matrix(name, ncol= 1)
+  resp_df <- NULL
+
 
   sensor <- substr(name, 2, 2)
   sat <- substr(name, 3, 4)
@@ -10,23 +12,29 @@ name_parser <- function(name) {
   aquisicao_mes <- substr(name, 22, 23)
   aquisicao_dia <- substr(name, 24, 25)
   band <- substr(name, nchar(name)-4, nchar(name)-4)
-  if(is.na(as.numeric(substr(name, nchar(name)-5, nchar(name)-5))) == FALSE) {
-    band <- paste0(substr(name, nchar(name)-5, nchar(name)-5),band)
-  }
-  #
-  # resp_df <- data.frame(sensor = "",
-  #                       sat = "",
-  #                       path = "",
-  #                       row = "",
-  #                       aquisicao_ano = "",
-  #                       aquisicao_mes ="",
-  #                       aquisicao_dia = "",
-  #                       band = "",
-  #                       stringsAsFactors=FALSE)
+  band <- sub("NA", "", paste0(as.numeric(substr(name, nchar(name)-5, nchar(name)-5)),band))
 
-  resp_df <- c(sensor,sat,path,row,aquisicao_ano,aquisicao_mes,aquisicao_dia,band)
 
-  # resp_df <- resp_df[2,]
+   resp_df <- c(name, sensor,
+                          sat,
+                          path,
+                          row,
+                          aquisicao_ano,
+                          aquisicao_mes,
+                          aquisicao_dia,
+                          band)
+
+
+  resp_df <- as.data.frame(matrix(resp_df,ncol = 9),stringAsFactors=FALSE)
+
+  colnames(resp_df) <- c("name", "sensor",
+                         "sat",
+                         "path",
+                         "row",
+                         "aquisicao_ano",
+                         "aquisicao_mes",
+                         "aquisicao_dia",
+                         "band")
 
   return(resp_df)
 }
@@ -60,6 +68,55 @@ band_target <- function(sat,name)  {
   return(as.numeric(ret))
 }
 
+get_table_files <- function(file_list) {
+  parser_name <- name_parser(file_list)
+  add_names <- add_band_name(parser_name)
+  return(add_names)
+}
+
+add_band_name <- function(parsed_name) {
+band_name <- NULL
+for(i in 1:nrow(parsed_name)) {
+    band_name <- rbind(band_name,band_name(as.character(parsed_name$sat[i]),as.character(parsed_name$band[i])))
+}
+
+  band_name <- cbind(parsed_name,band_name)
+
+  return(band_name)
+}
+
+band_name <- function(sat = 0,band = 0)  {
+
+  ret <- "ERROR"
+  sat <- as.numeric(sat)
+  band <- as.numeric(band)
+  if(is.na(sat) || is.na(band)) return("NO")
+
+
+    if((sat == 4 || sat == 5) && band == 1) ret <- "blue"
+    if((sat == 4 || sat == 5) && band == 2) ret <- "green"
+    if((sat == 4 || sat == 5) && band == 3) ret <- "red"
+    if((sat == 4 || sat == 5) && band == 4) ret <- "NIR"
+    if((sat == 4 || sat == 5) && band == 5) ret <- "SWIR"
+    if((sat == 4 || sat == 5) && band == 6) ret <- "TIR"
+    if((sat == 4 || sat == 5) && band == 7) ret <- "SWIR2"
+    if((sat == 4 || sat == 5) && band == 8) ret <- "pan"
+
+    if((sat == 7 || sat == 8) && band == 2) ret <- "blue"
+    if((sat == 7 || sat == 8) && band == 3) ret <- "green"
+    if((sat == 7 || sat == 8) && band == 4) ret <- "red"
+    if((sat == 7 || sat == 8) && band == 5) ret <- "NIR"
+    if((sat == 7 || sat == 8) && band == 6) ret <- "SWIR"
+    if((sat == 7 || sat == 8) && band == 7) ret <- "SWIR2"
+    if((sat == 7 || sat == 8) && band == 8) ret <- "pan"
+    if((sat == 7 || sat == 8) && band == 9) ret <- "cirrus"
+    if((sat == 7 || sat == 8) && band == 10) ret <- "TIRS1"
+    if((sat == 7 || sat == 8) && band == 11) ret <- "TIRS2"
+
+  return(ret)
+}
+
+
 ndvi <- function(nir_img, red_img)  {
   ret <- NULL
   ret <- (nir_img - red_img) / (nir_img + red_img)
@@ -80,4 +137,61 @@ area_water <- function(px_coords, image , sigma_value = .2) {
   return_percentage <- sum(px) / length(px) *100
 
   return(as.numeric(return_percentage))
+}
+
+get_water_size <- function(files_info) {
+
+  files_nir <- files_info[files_info$band_name == "NIR",]
+  files_red <- files_info[files_info$band_name == "red",]
+
+  results <- NULL
+  for(i in 1:nrow(files_nir)) {
+    nir_filename <- files_nir[i,]$name
+    nir_ano <- files_nir[i,]$aquisicao_ano
+    nir_mes <- files_nir[i,]$aquisicao_mes
+    nir_dia <- files_nir[i,]$aquisicao_dia
+    red_filename <- files_red[files_red$aquisicao_ano == nir_ano &
+                              files_red$aquisicao_mes == nir_mes &
+                              files_red$aquisicao_dia == nir_dia,]$name
+
+    nir_img <- load.image(paste0("represa/",nir_filename))
+    red_img <- load.image(paste0("represa/",red_filename))
+
+    img_ndvi <- ndvi(nir_img,red_img)
+    area_represa <- area_water(c(300,180),img_ndvi)
+
+    results_vector <- c(as.character(nir_ano),as.character(nir_mes),as.character(nir_dia),area_represa)
+    results <- rbind(results,results_vector)
+  }
+  results <- as.data.frame(results, stringsAsFactors = FALSE)
+  colnames(results) <- c("year","month","day","area")
+  # results <- as.numeric(results)
+  return(results)
+}
+
+get_all_areas <- function(files_info) {
+
+  files_nir <- files_info[files_info$band_name == "NIR",]
+  files_red <- files_info[files_info$band_name == "red",]
+
+  results <- list()
+  for(i in 1:nrow(files_nir)) {
+    nir_filename <- files_nir[i,]$name
+    nir_ano <- files_nir[i,]$aquisicao_ano
+    nir_mes <- files_nir[i,]$aquisicao_mes
+    nir_dia <- files_nir[i,]$aquisicao_dia
+    red_filename <- files_red[files_red$aquisicao_ano == nir_ano &
+                              files_red$aquisicao_mes == nir_mes &
+                              files_red$aquisicao_dia == nir_dia,]$name
+
+    nir_img <- load.image(paste0("represa/",nir_filename))
+    red_img <- load.image(paste0("represa/",red_filename))
+
+    img_ndvi <- ndvi(nir_img,red_img)
+
+    seg <- threshold(img_ndvi,thr = "12%")
+    px <- px.flood(seg, x = 300, y = 180, sigma=.2)
+    results[[paste0(nir_ano,nir_mes,nir_dia)]] <- px
+  }
+  return(results)
 }
--- artigo/sat/playgCLOUD.R
+++ artigo/sat/playgCLOUD.R
@@ -1,4 +1,4 @@
-PATH <- "C:/Users/Administrator/Documents/rPDI/artigo/sat"
+PATH <- "/home/yurisa2/rPDI/artigo/sat"
 
 setwd(PATH)
 
@@ -8,21 +8,26 @@ library("raster")
 source(file="functions.R")
 
 rm(file_list)
-file_list <- data.frame(filename = list.files(paste0(PATH,"/represa"), recursive = TRUE), stringsAsFactors = FALSE)
+file_list <- list.files(paste0(PATH,"/represa"), recursive = TRUE)
 
+files_info <- get_table_files(file_list)
+str(files_info)
 
-name_parser(file_list)
+water_size <- get_water_size(files_info)
+median_water_size <- median(as.numeric(water_size$area))
 
+water_size <- water_size[as.numeric(water_size$area) < (1.1 * median_water_size) &
+                        as.numeric(water_size$area) > (0.8 * median_water_size),  ]
 
+get_aa <- get_all_areas(files_info)
+# str(get_aa)
+# new_get_aa <- list()
+result_image <- get_aa[[paste0(water_size$year[1],water_size$month[1],water_size$day[1])]]
+for(i in 2:nrow(water_size)) {
+  # new_get_aa[[paste0(water_size$year[i],water_size$month[i],water_size$day[i])]] <- get_aa[[paste0(water_size$year[i],water_size$month[i],water_size$day[i])]]
 
-rm(imgs_df)
-imgs_df <- NULL
-
-for(i in file_list){
-  f_name <- name_parser(i)
-  target_band <- band_target(f_name$sat, "NIR")
-
+  result_image <-   result_image + get_aa[[paste0(water_size$year[i],water_size$month[i],water_size$day[i])]]
 }
 
-imgs_df <- (data.frame(imgs_df, stringsAsFactors = FALSE))
-str(imgs_df)
+
+plot(result_image)
