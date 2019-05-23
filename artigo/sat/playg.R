fca0f6ee21f5f8b20e520b4a06611e6b9d421961
yurisa2 <yuri@sa2.com.br>
2019-05-23 01:04:38 -0300

    Shitload of sat images L04

--- artigo/sat/playg.R
+++ artigo/sat/playg.R
@@ -1,10 +1,10 @@
-PATH <- "C:/Bitnami/wampstack-7.0.16-1/apache2/htdocs/rPDI/artigo/sat"
-PATH_IMG <- "C:/Users/Administrator/Documents/landsat"
+PATH <- "/root/rPDI/artigo/sat/"
+PATH_IMG <- "/root/itupararanga/"
 
 setwd(PATH)
 
 
-library("imager")
+#library("imager")
 library("raster")
 
 substrRight <- function(x, n){
@@ -15,7 +15,7 @@ file_list <- list.files(PATH_IMG, recursive = TRUE, full.names = TRUE)
 for(i in file_list){
   if(substrRight(i, 3) == "tif" || substrRight(i, 3) == "TIF") {
     rasto <- raster(i)
-    represa <- load.image(i)
+ #   represa <- load.image(i)
     represa <- extent(252000,275000,-2621000,-2608000)
     rasto_represa <- crop(rasto,represa)
     writeRaster(rasto_represa,paste0("represa/",basename(i)),options=c('TFW=NO'))
